23b59ee564730d4b566825447aa5dea1d8a0d3b6
Davi da Silva <davisllvsnt57@gmail.com>
2025-01-27 17:49:41 -0300

    Created the contentFields

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './global.css'
 import  styles from './App.module.css'
 import todoLogo from './assets/todo-logo.svg'
+import clipBoard from './assets/clipboard-logo.svg'
 import { PlusCircle } from "@phosphor-icons/react"
 
 export const App = () => {
@@ -16,17 +17,23 @@ export const App = () => {
         <div className={styles.formBoxes}>
             <form action="submit">
               <input type="text" placeholder="Adicione uma nova tarefa" />
-              <button type="submit">Criar <PlusCircle size={16} /></button>
+              <button type="submit">Criar <PlusCircle size={20} /></button>
             </form>
         </div>
 
-        <div>
-          <div>
+        <div className={styles.contentBoxes}>
+          <div className={styles.contentTexts}>
             <span>Tarefas Criadas <strong>0</strong></span>
-            <span>Tarefas Criadas <strong>0</strong></span>
+            <span>Concluídas <strong>0</strong></span>
           </div>
-          <div>
-
+          <div className={styles.contentFields}>
+            <img src={clipBoard} alt="Ícone de cópia" />
+            <p>
+              Você ainda não tem tarefas cadastradas
+            </p>
+            <span>
+              Crie tarefas e organize seus itens a fazer
+            </span>
           </div>
         </div>
       </div>
